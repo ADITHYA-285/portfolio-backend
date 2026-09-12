ded20425a5a3f3242e4d84f21ceed183d6375c98
Mohan G <symbolequal@gmail.com>
2026-09-12 23:06:57 +0530

    Connect backend to TiDB

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "portfolio"
+    host: process.env.TIDB_HOST || process.env.DB_HOST,
+    port: process.env.TIDB_PORT || 3306,
+    user: process.env.TIDB_USER || process.env.DB_USER,
+    password: process.env.TIDB_PASSWORD || process.env.DB_PASSWORD,
+    database: process.env.TIDB_DATABASE || process.env.DB_NAME,
+
+    ssl: process.env.TIDB_ENABLE_SSL === "true"
+        ? {
+            minVersion: "TLSv1.2"
+        }
+        : undefined
 });
 
 db.connect((err) => {
@@ -13,7 +20,7 @@ db.connect((err) => {
         return;
     }
 
-    console.log("MySQL connected successfully!");
+    console.log("MySQL/TiDB connected successfully!");
 });
 
 module.exports = db;
